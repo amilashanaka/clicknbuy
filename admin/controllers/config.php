594cd15f96ef4cc9f46ddd2fafe7562f909ec909
amilashanaka <dsa.amilashanaka@gmail.com>
2024-07-09 06:28:35 +0100

    update

--- admin/controllers/config.php
+++ admin/controllers/config.php
@@ -4,7 +4,7 @@ define('DB_HOST','localhost');
 
  define('DB_USER','root');
  define('DB_PASS', '');
- define('DB_NAME','chms');
+ define('DB_NAME','clicknbuy');
 
 /*define('DB_USER','u710400283_chm');
 define('DB_PASS', 'Chm@2024');
