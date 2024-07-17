b3910fd331630186567b6275c9addd7a8aa5e248
amilashanaka <dsa.amilashanaka@gmail.com>
2024-07-17 05:37:51 +0100

    Update config.php

--- admin/controllers/config.php
+++ admin/controllers/config.php
@@ -2,14 +2,14 @@
 // Db connection
 define('DB_HOST','localhost');
 
- define('DB_USER','root');
- define('DB_PASS', '');
- define('DB_NAME','clicknbuy');
+//  define('DB_USER','root');
+//  define('DB_PASS', '');
+//  define('DB_NAME','clicknbuy');
 
-/*define('DB_USER','u710400283_chm');
+define('DB_USER','u710400283_chm');
 define('DB_PASS', 'Chm@2024');
 define('DB_NAME','u710400283_chm');
-*/
+ 
 
 // URI parameters
 define('MAIN_ROUTE',2);
