f4db5b33bf1918267048b24fa1af6edfc6cf666c
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-08-06 16:33:26 +0530

    popular and brand corner updated

--- banner_2.php
+++ banner_2.php
@@ -1,6 +1,6 @@
 
 <div class="banner_2 sub-banner">
         <div class="container">
-            <a href="#"><img src="img/banner/ban1.jpg" style="width:1170px; height:220px" alt="images" class="img-responsive"></a>
+            <a href="#"><img src="img/banner/ad1.jpg" style="width:1170px; height:300px" alt="images" class="img-responsive"></a>
         </div>
     </div>
--- popular-product.php
+++ popular-product.php
@@ -17,14 +17,14 @@
                                     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
                                         <div class="product-item ver2">
                                             <div class="prod-item-img bd-style-2">
-                                                <a href="#"><img src="img/products/popular1.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                                <a href="#"><img src="img/products/popular1.avif" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                                 <div class="button">
                                                     <a href="#" class="addcart addcart-v2">ADD TO CART</a>
                                                     <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">Sony Alpha a5000 Mirrorless Digital Camera with 16-50mm...</a></h3>
+                                                <h3><a href="#"> Tesco Finest Baby Spinach (100g)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -34,8 +34,8 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price old">$299.6</span>
-                                                    <span class="price">$109.69</span>
+                                                    <span class="price old">£1.50</span>
+                                                    <span class="price">£1.00</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -50,7 +50,7 @@
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">Sony SEL1670Z Vario-Tessar T E 16-70mm F4 ZA OSS</a></h3>
+                                                <h3><a href="#"> Sainsbury's Spaghetti (500g)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -60,7 +60,7 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price black">$199.69</span>
+                                                    <span class="price black">£1.00</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -75,7 +75,7 @@
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">Beats EP Wired On-Ear Headphone - Rose</a></h3>
+                                                <h3><a href="#"> Iceland Frozen Peas (500g)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -85,7 +85,7 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price black">$199.69</span>
+                                                    <span class="price black">£0.75</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -93,14 +93,14 @@
                                     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 no-padding-right">
                                         <div class="product-item ver2">
                                             <div class="prod-item-img bd-style-2">
-                                                <a href="#"><img src="img/products/popular4.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                                <a href="#"><img src="img/products/popular4.avif" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                                 <div class="button">
                                                     <a href="#" class="addcart addcart-v2">ADD TO CART</a>
                                                     <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">PowerLead Pwah PL-N20 Bluetooth 4.0 Smart Watch...</a></h3>
+                                                <h3><a href="#">Tesco Semi-Skimmed Milk (4 pints)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -110,8 +110,8 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price old">$299.6</span>
-                                                    <span class="price">$109.69</span>
+                                                    <span class="price old">£1.50</span>
+                                                    <span class="price">£1.00</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -119,14 +119,14 @@
                                     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 no-padding-right">
                                         <div class="product-item ver2">
                                             <div class="prod-item-img bd-style-2">
-                                                <a href="#"><img src="img/products/popular5.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                                <a href="#"><img src="img/products/popular5.jpeg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                                 <div class="button">
                                                     <a href="#" class="addcart addcart-v2">ADD TO CART</a>
                                                     <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">Canon PIXMA Pro-100 Wireless Color Professional Inkjet...</a></h3>
+                                                <h3><a href="#">Heinz Tomato Ketchup (570g)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -136,8 +136,8 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price old">$299.6</span>
-                                                    <span class="price">$109.69</span>
+                                                    <span class="price old">£2.00</span>
+                                                    <span class="price">£1.50</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -145,14 +145,14 @@
                                     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 no-padding-right">
                                         <div class="product-item ver2">
                                             <div class="prod-item-img bd-style-2">
-                                                <a href="#"><img src="img/products/popular6.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                                <a href="#"><img src="img/products/popular6.jpeg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                                 <div class="button">
                                                     <a href="#" class="addcart addcart-v2">ADD TO CART</a>
                                                     <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                                 </div>
                                             </div>
                                             <div class="prod-info">
-                                                <h3><a href="#">Sony SBH52BLACK Sony Stereo Bluetooth Headset</a></h3>
+                                                <h3><a href="#"> Birds Eye Chicken Nuggets (300g)</a></h3>
                                                 <div class="ratingstar sm">
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -162,7 +162,7 @@
                                                     <span class="number">(12)</span>
                                                 </div>
                                                 <div class="p-price">
-                                                    <span class="price black">$199.69</span>
+                                                    <span class="price black">£2.50</span>
                                                 </div>
                                             </div>
                                         </div>
@@ -498,35 +498,35 @@
                         <div class="brand-list-v1">
                             <div class="product-item ver1 bd-style">
                                 <div class="prod-item-img">
-                                    <a href="#"><img src="img/products/canon_pixma.jpg" alt="images" class="img-responsive"></a>
+                                    <a href="#"><img src="img/products/popular1.avif" alt="images" class="img-responsive"></a>
                                 </div>
                                 <div class="prod-info">
-                                    <h3><a href="#">Canon MX492 Wireless All-IN-One Small...</a></h3>
+                                    <h3><a href="#">Heinz Tomato Ketchup (570g).</a></h3>
                                     <div class="p-price">
-                                        <span class="price black">$199.69</span>
+                                        <span class="price black">£1.50</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="product-item ver1 bd-style">
                                 <div class="prod-item-img">
-                                    <a href="#"><img src="img/products/canon.jpg" alt="images" class="img-responsive"></a>
+                                    <a href="#"><img src="img/products/onsale2.jpg" alt="images" class="img-responsive"></a>
                                 </div>
                                 <div class="prod-info">
-                                    <h3><a href="#">Canon EOS 6D Digital SLR Camera + EF...</a></h3>
+                                    <h3><a href="#">Walkers Crisps (Multipack).</a></h3>
                                     <div class="p-price">
-                                        <span class="price old">$299.6</span>
-                                        <span class="price">$109.69</span>
+                                        <span class="price old">£3.50</span>
+                                        <span class="price">£2.00</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="product-item ver1">
                                 <div class="prod-item-img">
-                                    <a href="#"><img src="img/products/canon2.jpg" alt="images" class="img-responsive"></a>
+                                    <a href="#"><img src="img/products/popular4.avif" alt="images" class="img-responsive"></a>
                                 </div>
                                 <div class="prod-info">
-                                    <h3><a href="#">Canon 0111C001 PowerShot SX610 HS, Wi-Fi...</a></h3>
+                                    <h3><a href="#">Tesco Semi-Skimmed Milk (4 pints)</a></h3>
                                     <div class="p-price">
-                                        <span class="price black">$199.69</span>
+                                        <span class="price black">£1.00</span>
                                     </div>
                                 </div>
                             </div>
@@ -534,28 +534,28 @@
                         <div class="brand-list-v1">
                             <div class="product-item ver1 bd-style">
                                 <div class="prod-item-img">
-                                    <a href="#"><img src="img/products/imac.jpg" alt="images" class="img-responsive"></a>
+                                    <a href="#"><img src="img/products/brand4.jpg" alt="images" class="img-responsive"></a>
                                 </div>
                                 <div class="prod-info">
-                                    <h3><a href="#">Apple iMac MK462LL/A 27-Inch Retina 5K...</a></h3>
+                                    <h3><a href="#">Kellogg's Coco Pops (375g)</a></h3>
                                     <div class="p-price">
-                                        <span class="price old">$299.6</span>
-                                        <span class="price">$109.69</span>
+                                        <span class="price old">£3.00</span>
+                                        <span class="price">£2.50</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="product-item ver1 bd-style">
                                 <div class="prod-item-img">
-                                    <a href="#"><img src="img/products/iwatch.jpg" alt="images" class="img-responsive"></a>
+                                    <a href="#"><img src="img/products/brand5.png" alt="images" class="img-responsive"></a>
                                 </div>
                                 <div class="prod-info">
-                                    <h3><a href="#">Apple Watch Series 1 38mm Smartwatch...</a></h3>
+                                    <h3><a href="#">Coca-Cola (2 Litre)</a></h3>
                                     <div class="p-price">
-                                        <span class="price black">$199.69</span>
+                                        <span class="price black">£1.20</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="product-item ver1">
+                            <!-- <div class="product-item ver1">
                                 <div class="prod-item-img">
                                     <a href="#"><img src="img/products/macmini2.jpg" alt="images" class="img-responsive"></a>
                                 </div>
@@ -566,7 +566,7 @@
                                         <span class="price">$109.69</span>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                     </aside>
                 </div>
