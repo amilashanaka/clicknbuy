b1754d4926f6a3bf62312133783f99795acba220
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-08-06 16:05:06 +0530

    Feature  page updated , banner updated

--- featured-product.php
+++ featured-product.php
@@ -5,10 +5,10 @@
                 <h3 class="pull-left">featured products</h3>
                 <ul class="otherr-link pull-right">
                     <li class="active"><a data-toggle="pill" href="#all">all</a></li>
-                    <li><a data-toggle="pill" href="#elec">Electronic</a></li>
-                    <li><a data-toggle="pill" href="#fashion">Fashion        </a></li>
-                    <li><a data-toggle="pill" href="#it">IT        </a></li>
-                    <li><a data-toggle="pill" href="#food">Food & Drink</a></li>
+                    <li><a data-toggle="pill" href="#elec">Fresh & Produce</a></li>
+                    <li><a data-toggle="pill" href="#fashion">Frozen Foods</a></li>
+                    <li><a data-toggle="pill" href="#it">Essentials</a></li>
+                    <li><a data-toggle="pill" href="#food">Dairy & Bakery</a></li>
                 </ul>
                 <div class="clearfix"></div>
             </div>
@@ -19,14 +19,14 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/feature1.jpeg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature1.avif" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Sony Xperia X Compact - Unlocked Smartphone...</a></h3>
+                                        <h3><a href="#">Tesco Finest British Strawberries (250g punnet)</a></h3>
                                         <div class="ratingstar sm">
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -36,7 +36,8 @@
                                             <span class="number">(12)</span>
                                         </div>
                                         <div class="prod-price">
-                                            <span class="price black">$212.20</span>
+                                            <span class="price old">£3.00</span>
+                                            <span class="price black">£2.00</span>
                                         </div>
                                     </div>
                                 </div>
@@ -44,14 +45,14 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/feature2.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature2.avif" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Sony MDRXB950BT/B Ex .fa-1tra Bass Bluetooth Headphones...</a></h3>
+                                        <h3><a href="#">Heinz Baked Beans (410g)</a></h3>
                                         <div class="ratingstar sm">
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -61,8 +62,8 @@
                                             <span class="number">(12)</span>
                                         </div>
                                         <div class="p-price">
-                                            <span class="price old">$699.6</span>
-                                            <span class="price">$510.02</span>
+                                            <span class="price old"> £0.80</span>
+                                            <span class="price"> £0.60</span>
                                         </div>
                                     </div>
                                 </div>
@@ -70,14 +71,14 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/feature3.avif" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature3.jpeg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Samsung UN65KS8000 65-Inch 4K Ultra HD Smart LED TV...</a></h3>
+                                        <h3><a href="#">Birds Eye Fish Fingers (300g)</a></h3>
                                         <div class="ratingstar sm" style="display:none;">
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -87,8 +88,8 @@
                                             <span class="number">(12)</span>
                                         </div>
                                         <div class="p-price margin--top">
-                                            <span class="price old">$399.6</span>
-                                            <span class="price">$299.69</span>
+                                            <span class="price old">£2.50</span>
+                                            <span class="price">£2.00</span>
                                         </div>
                                     </div>
                                 </div>
@@ -96,14 +97,14 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/feature4.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature4.jpeg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Sony a7 Full-Frame Mirrorless Digital Camera...</a></h3>
+                                        <h3><a href="#">Tesco Free Range Eggs (6 pack)</a></h3>
                                         <div class="ratingstar sm">
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -113,7 +114,7 @@
                                             <span class="number">(12)</span>
                                         </div>
                                         <div class="p-price">
-                                            <span class="price black">$199.69</span>
+                                            <span class="price black">£1.00</span>
                                         </div>
                                     </div>
                                 </div>
@@ -121,18 +122,18 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/feature5.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature55.jpg" style="width:200px; height:200px" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Apple iPad 4 16GB 9.7" Retina Display WiFi Bluetooth...</a></h3>
+                                        <h3><a href="#">Sainsbury's Basic White Bread (800g)</a></h3>
                                         <div class="ratingstar" style="display:none;"><span class="number">(12)</span></div>
                                         <div class="p-price margin--top">
-                                            <span class="price old">$299.6</span>
-                                            <span class="price">$109.69</span>
+                                            <span class="price old">£1.00</span>
+                                            <span class="price">£0.75</span>
                                         </div>
                                     </div>
                                 </div>
@@ -146,14 +147,14 @@
                             <div class="col-md-15 col-sm-4 col-xs-6">
                                 <div class="product-item ver2">
                                     <div class="prod-item-img bd-style-2">
-                                        <a href="#"><img src="img/products/smarphone.jpg" alt="images" class="img-responsive"></a>
+                                        <a href="#"><img src="img/products/feature6.jpg" alt="images" class="img-responsive"></a>
                                         <div class="button">
                                             <a href="#" class="addcart">ADD TO CART</a>
                                             <a href="#" class="view"><i class="fa fa-eye" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
                                     <div class="prod-info">
-                                        <h3><a href="#">Sony Xperia X Compact - Unlocked Smartphone...</a></h3>
+                                        <h3><a href="#">Iceland Frozen Chicken Breast (500g)</a></h3>
                                         <div class="ratingstar sm">
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                             <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -163,7 +164,7 @@
                                             <span class="number">(12)</span>
                                         </div>
                                         <div class="prod-price">
-                                            <span class="price black">$212.20</span>
+                                            <span class="price black"> £3.00</span>
                                         </div>
                                     </div>
                                 </div>
