0f51ae393d07be83dc1bf20c62b2756032cc60b2
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-08-06 15:40:30 +0530

    Onsale page updated

--- bigdeal.php
+++ bigdeal.php
@@ -6,7 +6,7 @@
                     <div class="col-md-7 col-sm-12">
                         <div class="product-item ver4 top-inner">
                             <div class="prod-item-img">
-                                <a href="#"><img src="img/products/onsale1.jpg"  alt="images" class="img-reponsive"></a>
+                                <a href="#"><img src="img/products/onsale1.jpeg"  alt="images" class="img-reponsive"></a>
                                 <div class="prod-choose">
                                     <div class="prod-color">
                                         <span class="dot"></span>
@@ -14,14 +14,14 @@
                                         <span class="dot green"></span>
                                     </div>
                                     <div class="prod-price">
-                                        <span class="price old">$200.50</span>
-                                        <span class="price-ver2 price-lg">$125.00</span>
+                                        <span class="price old">£2.00</span>
+                                        <span class="price-ver2 price-lg">£1.50</span>
                                         <span class="productPriceDiscount">Save<br><span class="strong">$75.5</span></span>
                                     </div>
                                 </div>
                             </div>
                             <div class="prod-info">
-                                <h3><a class="prod-name" href="#" title="">Sony Smartwatch 3 - 2016</a></h3>
+                                <h3><a class="prod-name" href="#" title="">Heinz Tomato Ketchup (570g)</a></h3>
                                 <p class="brand">SONY</p>
                                 <div class="ratingstar">
                                     <a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a>
@@ -33,13 +33,12 @@
                                 </div>
                                 <div class="prod-description">
                                     <ul>
-                                        <li>Plays all your music, fomat type (WAV, MP4, ...)</li>
-                                        <li>Fills the room with immersive</li>
-                                        <li>Allows hands-free</li>
-                                        <li>Controls lights, switches</li>
+                                        <li>Adds that perfect tangy kick to your meals</li>
+                                        <li>Made with vine-ripened tomatoes for a rich, delicious flavor</li>
+                                        <li>A pantry staple that's perfect for burgers, fries, and more</li>
                                     </ul>
                                 </div>
-                                <div class="countdown" data-countdown="countdown" data-date="08-31-2018-00-00-00">
+                                <div class="countdown" data-countdown="countdown">
                                 </div>
                             </div>
                         </div>
@@ -47,10 +46,10 @@
                     <div class="col-md-5 col-sm-12 ">
                         <div class="product-item ver1 padding--bottom margin-r">
                             <div class="prod-item-img set--width">
-                                <a class="image" href="#"><img src="img/products/onsale2.jpeg" alt="images" class="img-responsive"></a>
+                                <a class="image" href="#"><img src="img/products/onsale2.jpg" alt="images" class="img-responsive"></a>
                             </div>
                             <div class="prod-info ver1">
-                                <h3><a href="#" title="">iPad Pro MLMX2CL/A (MLMX2LL/A) 9.7-inch</a></h3>
+                                <h3><a href="#" title="">Walkers Crisps (Multipack)</a></h3>
                                 <div class="ratingstar sm">
                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -60,8 +59,8 @@
                                     <span class="number">(12)</span>
                                 </div>
                                 <div class="prod-price">
-                                    <span class="price old">$299.6</span>
-                                    <span class="price">$210.25</span>
+                                    <span class="price old">£3.00</span>
+                                    <span class="price">£2.00</span>
                                 </div>
                             </div>
                             <div class="label label-2 blue">New</div>
@@ -71,7 +70,7 @@
                                 <a class="image" href="#"><img src="img/products/onsale3.avif"  alt="images" class="img-responsive"></a>
                             </div>
                             <div class="prod-info ver1">
-                                <h3><a href="#" title="">Motorola Moto 360 Sport - 45mm, Black</a></h3>
+                                <h3><a href="#" title="">Tesco Finest Free-Range Eggs (12 pack)</a></h3>
                                 <div class="ratingstar sm">
                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
                                     <a href="#"><i class="fa fa-star-o fa-1" aria-hidden="true"></i></a>
@@ -81,8 +80,8 @@
                                     <span class="number">(12)</span>
                                 </div>
                                 <div class="prod-price">
-                                    <span class="price old">$399.6</span>
-                                    <span class="price">$327.50</span>
+                                    <span class="price old">£3.50</span>
+                                    <span class="price">£2.50</span>
                                 </div>
                             </div>
                             <div class="label label-2 red">Hot</div>
