a4684bfae5cce07a40c4488d41d7f10176caf271
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-08-06 17:11:38 +0530

    brand updated

--- category.php
+++ category.php
@@ -3,59 +3,59 @@
             <div class="row">
                 <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="heading-v3 dif">
-                        <img src="img/smicon.png" alt="images" class="img-responsive">
-                        <span>Smartphone</span>
+                        <img src="img/categoryicon1.png" style="width:30px; height:30px" alt="images" class="img-responsive">
+                        <span>Fresh & Produce</span>
                     </div>
                     <div class="product-item ver3">
                         <div class="photo">
-                            <a href="#"><img src="img/iphone6.png" alt="images" class="img-responsive"></a>
+                            <a href="#"><img src="img/category1.avif" style="width:125px; height:145px" alt="images" class="img-responsive"></a>
                         </div>
                         <ul>
-                            <li><a href="#">phones</a></li>
-                            <li><a href="#">android</a></li>
-                            <li><a href="#">google phones</a></li>
-                            <li><a href="#">apple phones</a></li>
-                            <li><a href="#">batteries</a></li>
-                            <li><a href="#">charges</a></li>
+                            <li><a href="#">Fruits</a></li>
+                            <li><a href="#">Vegetables</a></li>
+                            <li><a href="#">Herbs & Spices</a></li>
+                            <li><a href="#">Salad Greens</a></li>
+                            <li><a href="#">Pre-Cut Fruits & Veggies</a></li>
+                            <li><a href="#">Mushrooms & Onions</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="heading-v3">
-                        <img src="img/laptopicon.png" alt="images" class="img-responsive">
-                        <span>LAPTOP</span>
+                        <img src="img/categoryicon2.png" style="width:30px; height:30px" alt="images" class="img-responsive">
+                        <span>Pantry Staples</span>
                     </div>
                     <div class="product-item ver3">
                         <div class="photo">
-                            <a href="#"><img src="img/laptop.png" alt="images" class="img-responsive"></a>
+                            <a href="#"><img src="img/category2.jpg" style="width:125px; height:145px" alt="images" class="img-responsive"></a>
                         </div>
                         <ul>
-                            <li><a href="#">phones</a></li>
-                            <li><a href="#">android</a></li>
-                            <li><a href="#">google phones</a></li>
-                            <li><a href="#">apple phones</a></li>
-                            <li><a href="#">batteries</a></li>
-                            <li><a href="#">charges</a></li>
+                            <li><a href="#">Canned Goods</a></li>
+                            <li><a href="#">Dry Goods</a></li>
+                            <li><a href="#">Sauces & Condiments</a></li>
+                            <li><a href="#">Oils & Vinegars</a></li>
+                            <li><a href="#">Spices & Seasonings</a></li>
+                            <li><a href="#">Baking Supplies</a></li> 
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="heading-v3">
-                        <img src="img/camicon.png" alt="images" class="img-responsive">
-                        <span>CAMERA</span>
+                        <img src="img/categoryicon3.png" style="width:30px; height:30px" alt="images" class="img-responsive">
+                        <span>Frozen Foods</span>
                     </div>
                     <div class="product-item ver3">
                         <div class="photo">
-                            <a href="#"><img src="img/cam.png" alt="images" class="img-responsive"></a>
+                            <a href="#"><img src="img/category3.jpeg" style="width:125px; height:145px" alt="images" class="img-responsive"></a>
                         </div>
                         <ul>
-                            <li><a href="#">phones</a></li>
-                            <li><a href="#">android</a></li>
-                            <li><a href="#">google phones</a></li>
-                            <li><a href="#">apple phones</a></li>
-                            <li><a href="#">batteries</a></li>
-                            <li><a href="#">charges</a></li>
-                        </ul>
+                            <li><a href="#">Frozen Fruits & Vegetables</a></li>
+                            <li><a href="#">Frozen Meals</a></li>
+                            <li><a href="#">Frozen Meat</a></li>
+                            <li><a href="#">Frozen Seafood</a></li>
+                            <li><a href="#">Frozen Bread & Bakery</a></li>
+                            <li><a href="#">Frozen Desserts</a></li>
+                        </ul> 
                     </div>
                 </div>
             </div>
